cdc7c38fedd40a5524a24cb81a7015683511746b
vinaypalnati <vinaypalnati6174@gmail.com>
2020-03-19 12:41:47 +0530

    created post model

--- vinaysite/blog/models.py
+++ vinaysite/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
+class Post(models.Model):
+    author=models.ForeignKey('auth.User')
+    title=models.CharField(max_length='45')
+    text=models.TextField()
+    published_date=models.DateTimeField(blank=True,null=True)
+    create_date=models.DateTimeField(default=timezone.now())
+
+    def publish(self):
+        return self.published_date= timezone.now()
+        self.save()
